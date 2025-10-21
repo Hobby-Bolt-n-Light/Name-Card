3e71b20b6c3720139704f46878e884b6cb71d00d
Hobby-Bolt-n-Light <124792396+Hobby-Bolt-n-Light@users.noreply.github.com>
2025-10-21 22:08:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function noscroll(e){
  
 document.addEventListener('touchmove', noscroll, {passive: false});
 document.addEventListener('wheel', noscroll, {passive: false});
+
+
+let url = new URL(window.location.href);
+let params = url.searchParams;
+console.log(params.get('id'));
+
+let userName = document.getElementById('friend');
+userName.innerHTML = params.get('id'); // innerHTMLでhtmlを追加
